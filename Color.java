0e49145bcf6fa3edee0748300c040b3d85b5c0b4
Joaquín Alejandro España Sánchez <joaquinalejandroespana@gmail.com>
2020-11-20 13:10:33 +0100

    Fichero para dar mejor aspecto a la interfaz

---
+++ Color.java
@@ -0,0 +1,13 @@
+public class Color {
+    // Fichero para dar mejor aspecto a la interfaz que tendrá el usuario
+    // Fuente del código: https://dryant.com/programacion/como-poner-el-texto-de-color-en-la-consola-o-terminal-ejemplo-en-java/
+    public String black="\033[30m"; 
+    public String red="\033[31m"; 
+    public String green="\033[32m"; 
+    public String yellow="\033[33m"; 
+    public String blue="\033[34m"; 
+    public String purple="\033[35m"; 
+    public String cyan="\033[36m"; 
+    public String white="\033[37m";
+    public String reset="\u001B[0m"; 
+}
